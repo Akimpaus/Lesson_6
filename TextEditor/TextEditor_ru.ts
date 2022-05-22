1de3676738d1985b0b8b32fb7834fe6bb7fd2e07
Аким Клеймёнов <akimpus2018@gmail.com>
2022-05-22 17:32:45 +0300

    First commit.

---
+++ TextEditor/TextEditor_ru.ts
@@ -0,0 +1,149 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Text editor</source>
+        <translation>Текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="47"/>
+        <location filename="mainwindow.cpp" line="150"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="60"/>
+        <location filename="mainwindow.cpp" line="164"/>
+        <source>Reference</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <location filename="mainwindow.cpp" line="157"/>
+        <source>View</source>
+        <translation>Вид</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <location filename="mainwindow.cpp" line="158"/>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="77"/>
+        <location filename="mainwindow.cpp" line="161"/>
+        <source>Decoration</source>
+        <translation>Оформление</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="91"/>
+        <location filename="mainwindow.cpp" line="151"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="96"/>
+        <location filename="mainwindow.cpp" line="153"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="101"/>
+        <location filename="mainwindow.cpp" line="154"/>
+        <source>Save as</source>
+        <translation>Сохранить как</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="106"/>
+        <location filename="mainwindow.cpp" line="165"/>
+        <source>About programm</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="111"/>
+        <location filename="mainwindow.cpp" line="156"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="116"/>
+        <location filename="mainwindow.cpp" line="152"/>
+        <source>Open read-only</source>
+        <translation>Открыть только для чтения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="121"/>
+        <location filename="mainwindow.cpp" line="159"/>
+        <source>Russian</source>
+        <translation>Русский</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="126"/>
+        <location filename="mainwindow.cpp" line="160"/>
+        <source>English</source>
+        <translation>Английский</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="131"/>
+        <location filename="mainwindow.cpp" line="163"/>
+        <source>Bright</source>
+        <translation>Светлое</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="136"/>
+        <location filename="mainwindow.cpp" line="162"/>
+        <source>Dark</source>
+        <translation>Темное</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="141"/>
+        <location filename="mainwindow.cpp" line="155"/>
+        <location filename="mainwindow.cpp" line="236"/>
+        <source>Print</source>
+        <translation>Печать</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="59"/>
+        <source>Data cannot be retrieved.</source>
+        <translation>Данные не могут быть извлечены.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="62"/>
+        <source>About program</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="90"/>
+        <source>Open file</source>
+        <translation>Открытие файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="96"/>
+        <source>File opening error.</source>
+        <translation>Ошибка открытия файла.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="96"/>
+        <source>It is impossible to make a read.</source>
+        <translation>Невозможно произвести чтение.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="178"/>
+        <source>Error saving the file.</source>
+        <translation>Ошибка сохранения файла.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="178"/>
+        <source>It is impossible to make a write.</source>
+        <translation>Невозможно произвести запись.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="190"/>
+        <source>Save file.</source>
+        <translation>Сохранение файла.</translation>
+    </message>
+</context>
+</TS>
---
+++ TextEditor/mainwindow.h
@@ -0,0 +1,88 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QString>
+#include <QTranslator>
+#include <QShortcut>
+#include <QKeySequence>
+#include <QPrinter>
+#include <QPrintDialog>
+#include <QPainter>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+protected:
+
+    void setLanguage_(const QString& language);
+    void setReadOnly_(const bool value);
+    void setSaveAbility_(const bool ability);
+    void saveFile_(const QString& file_name);
+    void saveFileAs_();
+    void openFile_(const QString& file_name);
+    bool openFileAs_();
+    void print_(QPrinter& printer) const;
+    void changeEvent(QEvent* event) override;
+
+    Ui::MainWindow *ui;
+    QString m_file_name;
+    QTranslator m_translator;
+
+    struct Shortcuts
+    {
+        QShortcut open;
+        QShortcut save;
+        QShortcut exit;
+
+        Shortcuts(QObject* parent) :
+            open(parent),
+            save(parent),
+            exit(parent)
+        {
+            setDefaultKeys();
+        }
+
+        void setDefaultKeys()
+        {
+            open.setKey(QKeySequence(Qt::CTRL | Qt::Key_O));
+            save.setKey(QKeySequence(Qt::CTRL | Qt::Key_S));
+            exit.setKey(QKeySequence(Qt::CTRL | Qt::Key_Q));
+        }
+
+    };
+    Shortcuts m_shortcuts;
+
+private slots:
+    void on_decor_dark_triggered();
+
+    void on_decor_bright_triggered();
+
+    void on_open_triggered();
+
+    void on_open_only_for_read_triggered();
+
+    void on_save_triggered();
+
+    void on_save_as_triggered();
+
+    void on_about_programm_triggered();
+
+    void on_exit_triggered();
+
+    void on_language_rus_triggered();
+
+    void on_language_eng_triggered();
+
+    void on_print_triggered();
+};
+#endif // MAINWINDOW_H
